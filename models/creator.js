8ae9808c6641641553427a3eeb316b0d6b968b2c
zZissou <kyle.edward.russell@gmail.com>
2016-11-13 19:46:45 -0800

    made correction from authors to creators and created the Schema variables for show.js

--- models/creator.js
+++ models/creator.js
@@ -1,33 +1,10 @@
 var mongoose = require('mongoose'),
   Schema = mongoose.Schema;
-  Creator = require('./creator');
 
-var CharacterSchema = new Schema({
-  name: String,
+var CreatorSchema = new Schema({
+  name: String
 });
 
-var ShowSchema = new Schema({
-  title: String,
-  mainCharacter, String,
-  creator: {type: Schema.Types.ObjectId, ref: 'Creator'},
-  image: String,
-  releaseDate: String,
-  seasons: String,
-  characters: [CharacterSchema]
-});
+var Creator = mongoose.model('Creator', CreatorSchema);
 
-var Show = mongoose.model('Show', BookSchema);
-module.exports = Show;
-
-
-
-// var mongoose = require('mongoose'),
-//   Schema = mongoose.Schema;
-
-// var CampsiteSchema = new Schema({
-//   description: String
-// });
-
-// var Campsite = mongoose.model('Campsite', CampsiteSchema);
-
-// module.exports = Campsite;
+module.exports = Creator;
