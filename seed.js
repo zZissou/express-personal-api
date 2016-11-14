9ee7f5d5d42b0860e816abfab20916755658bd1f
zZissou <kyle.edward.russell@gmail.com>
2016-11-13 19:28:33 -0800

    filled out functions for finding and creating shows and creators in seedj.s

--- seed.js
+++ seed.js
@@ -2,29 +2,66 @@
 // simply run: `node seed.js` from the root of this project folder.
 var db = require('./models')
 
-
-var planet_list = [
+var show_list = [
   {
-  name:+
-  image:+
-  system:+
-  suns:
-  atmosphere:
-  climate:
-  nativeSpecies:+
-  population:+
-  Capital:+
-  majorExports:
+  title:"",
+  creator:"",
+  image:"",
+  seasons:"",
+  releaseDate:"",
+  },
+  {
+  title:
+  creator:
+  image:
+  network:
+  releaseDate:
+  seasons:
   }
-];
-
-var system_list = [
-  name:
-  region:
-  planets:
 ]
-}
 
+var creators_list:
+  {
+    name:"",
+
+  }
+
+db.Creator.remove({}, function(err, creators) {
+  console.log('removed all authors');
+  db.Creator.create(creators_list, function(err, creators){
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log('recreated all creators');
+    console.log("created", creators.length, "creators");
+
+    db.Show.remove({}, function(err, books){
+      console.log('removed all shows');
+      shows_list.forEach(function (showData) {
+        var show = new db.Show({
+          title: showData.title,
+          image: showData.image,
+          releaseDate: showData.releaseDate
+        });
+        db.Creator.findOne({name: showData.creator}, function(err, foundCreator) {
+          console.log('found creator ' + foundCreator.name + ' for show ' + show.title);
+          if (err) {
+            console.log(err);
+            return;
+          }
+          show.creator = foundCreator;
+          show.save(function(err, savedShow){
+            if (err) {
+              return console.log(err);
+            }
+            console.log('saved '+ savedShow.title + ' by ' + foundCreator.name);
+          });
+        };
+      });
+    });
+  });
+});
 // var db = require('./models');
 
 // var new_campsite = {description: "Sharp rocks. Middle of nowhere."}
